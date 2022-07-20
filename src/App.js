158e41a7f2b2df3430137bcec9267bb5d7b9cfb3
Jovan Stankovic <stankovicjovan1994@gmail.com>
2022-07-20 21:26:06 +0200

    [STYLE] setting up initial style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Container, Stack, Button } from "react-bootstrap";
+import classes from "./App.module.css";
+
 function App() {
-  return <>"asd"</>;
+  return (
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div className={classes.card}>ASD</div>
+    </Container>
+  );
 }
 
 export default App;
